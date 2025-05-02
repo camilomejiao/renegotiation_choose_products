dbcea393859a077c1852fabab319504e9493b95a
cmejia <camiloa.mejiao@gmail.com>
2025-05-02 18:22:41 -0500

    New validations have been added for delivery approvals, new statuses and buttons, it was necessary to remove roles that were no longer part of the validation process

--- src/components/layout/private/deliveries/Deliveries.jsx
+++ src/components/layout/private/deliveries/Deliveries.jsx
@@ -1,8 +1,8 @@
-import {useEffect, useRef, useState} from "react";
-import {useNavigate, useOutletContext, useParams} from "react-router-dom";
+import { useEffect, useRef, useState } from "react";
+import { useNavigate, useOutletContext, useParams } from "react-router-dom";
 import Select from "react-select";
-import {Button, Col, Container, Form, Row, Spinner} from "react-bootstrap";
-import {FaCheck, FaCheckDouble, FaFilePdf, FaPencilAlt, FaTrash} from "react-icons/fa";
+import { Button, Col, Container, Form, Row, Spinner } from "react-bootstrap";
+import {FaCheck, FaCheckDouble, FaClipboardCheck, FaFilePdf, FaPencilAlt, FaTimes, FaTrash} from "react-icons/fa";
 import printJS from "print-js";
 import { DataGrid } from "@mui/x-data-grid";
 
@@ -80,6 +80,7 @@ export const Deliveries = () => {
 
     //Listado de entregas al titular
     const getListDeliveriesToUser = async (cubId) => {
+        setIsLoading(true);
         try {
             const { data, status} = await deliveriesServices.searchDeliveriesToUser(cubId);
             if(status === ResponseStatusEnum.OK) {
@@ -87,6 +88,8 @@ export const Deliveries = () => {
             }
         } catch (error) {
             console.error("Error fetching deliveries:", error);
+        } finally {
+            setIsLoading(false);
         }
     }
 
@@ -96,8 +99,8 @@ export const Deliveries = () => {
             if(status === ResponseStatusEnum.OK) {
                 return {
                     urlFile: Array.isArray(data?.archivos) && data.archivos.length === 0 ? parseInt(0) : data?.archivos[0]?.ruta,
-                    approvedTechnical: data?.fecha_aprobado_tecnica === null ? parseInt(0) : data?.fecha_aprobado_tecnica,
-                    approvedTerritorial: data?.fecha_aprobado_territorial === null ? parseInt(0) : data?.fecha_aprobado_territorial,
+                    approvedTechnical: data?.aprobado_tecnica,
+                    approvedTerritorial: data?.aprobado_territorial,
                 }
             }
         } catch (error) {
@@ -116,8 +119,8 @@ export const Deliveries = () => {
                     supplier: row.proveedor,
                     evidencePdf: { urlFile: deliveryIdInfo?.urlFile },
                     actions: {
-                        approvedTechnical: deliveryIdInfo?.approvedTechnical ?? 0,
-                        approvedTerritorial: deliveryIdInfo?.approvedTerritorial ?? 0,
+                        approvedTechnical: deliveryIdInfo?.approvedTechnical,
+                        approvedTerritorial: deliveryIdInfo?.approvedTerritorial,
                     }
                 };
             })
@@ -137,9 +140,35 @@ export const Deliveries = () => {
         }
     }
 
+    //
     const isButtonDisabled = (row) => {
-        return row.actions.approvedTerritorial === 1;
+        if((row.actions.approvedTerritorial === true || row.actions.approvedTerritorial === false) && (userAuth.rol_id === RolesEnum.TERRITORIAL_LINKS || userAuth.rol_id === RolesEnum.TECHNICAL)) {
+            return true;
+        }
     };
+
+    //
+    const isButtonDisabledTecnical = (row) => {
+        const { approvedTerritorial, approvedTechnical } = row.actions;
+
+        if(userAuth.rol_id === RolesEnum.TECHNICAL &&
+            (approvedTerritorial === true || approvedTechnical === true || approvedTechnical === false)) {
+            return true;
+        }
+        return false;
+    };
+
+
+    //
+    const renderApprovalIcon = (status) => {
+        if (status === 1 || status === true) {
+            return <FaCheck title="Aprobado" style={{ backgroundColor: "#FFF", color: "#28A745" }} />;
+        }
+        if (status === 0 || status === false) {
+            return <FaTimes title="Denegado" style={{ color: "#DC3545" }} />;
+        }
+        return <FaClipboardCheck title="Pendiente de revisión" style={{ color: "#FFC107" }} />;
+    }
 
     // Definición de las columnas de entregas
     const deliveryColumns = [
@@ -273,19 +302,22 @@ export const Deliveries = () => {
                                 >
                                     <FaTrash/>
                                 </Button>
-                                <Button
-                                    variant="success"
-                                    size="sm"
-                                    onClick={() => {
-                                        setSelectedDeliveryId(params.row.id);
-                                        setOpenModal(true);
-                                    }}
-                                >
-                                    <FaCheck/>
-                                </Button>
+                                {params.row.evidencePdf?.urlFile !== 0 && (
+                                    <Button
+                                        style={{ backgroundColor: "#FFF", marginRight: "10px" }}
+                                        size="sm"
+                                        onClick={() => {
+                                            setSelectedDeliveryId(params.row.id);
+                                            setOpenModal(true);
+                                        }}
+                                        disabled={isButtonDisabled(params.row)}
+                                    >
+                                        {renderApprovalIcon(params.row.actions?.approvedTerritorial)}
+                                    </Button>
+                                )}
                             </>
                         )}
-                        {((userAuth.rol_id === RolesEnum.TECHNICAL || userAuth.rol_id === RolesEnum.ADMIN) && params?.row?.action?.approvedTerritorial === 1) && (
+                        {((userAuth.rol_id === RolesEnum.TECHNICAL || userAuth.rol_id === RolesEnum.ADMIN) && params.row.evidencePdf?.urlFile !== 0) && (
                             <>
                                 <Button
                                     variant="success"
@@ -294,6 +326,7 @@ export const Deliveries = () => {
                                         setSelectedDeliveryId(params.row.id);
                                         setOpenModal(true);
                                     }}
+                                    disabled={isButtonDisabledTecnical(params.row)}
                                 >
                                     <FaCheckDouble/>
                                 </Button>
@@ -504,7 +537,7 @@ export const Deliveries = () => {
     }
 
     const handleApproveByAudit = async () => {
-        if (!action && comment.trim() === "") {
+        if (!action || comment.trim() === "") {
             showError("Validación", "Debes seleccionar una acción o escribir un comentario.");
             return;
         }
@@ -518,6 +551,7 @@ export const Deliveries = () => {
 
             const { status } = await deliveriesServices.approveDelivery(selectedDeliveryId, payload);
             if (status === ResponseStatusEnum.OK) {
+                getListDeliveriesToUser(params.id);
                 showAlert("Bien hecho!", `Entrega ${action === 'approve' ? 'Aprobada' : 'Denegada'} exitosamente!`);
                 handleCloseModalApproved();
             }
@@ -698,26 +732,37 @@ export const Deliveries = () => {
                 <div className="deliveries-banner">
                     <Container>
                         <Row className="justify-content-start align-items-center mt-4">
-                            {(userAuth.rol_id === RolesEnum.TERRITORIAL_LINKS || userAuth.rol_id === RolesEnum.ADMIN || userAuth.rol_id === RolesEnum.SUPERVISION) && (
-                                <Col xs={12} md={6} className="d-flex align-items-center">
-                                    <Select
-                                        value={selectedSupplier}
-                                        onChange={(selectedOption) => setSelectedSupplier(selectedOption)}
-                                        options={suppliers?.map((opt) => ({ value: opt.id, label: opt.nombre }))}
-                                        placeholder="Selecciona una compañía"
-                                        classNamePrefix="custom-select"
-                                        className="custom-select w-100"
-                                    />
-                                </Col>
-                            )}
-                            <Col xs={12} md={6} className="d-flex align-items-center justify-content-md-start justify-content-center">
-                                <button onClick={handleCreateDeliveries} className="deliveries-button deliveries d-flex align-items-center">
-                                    <img src={imgFrame2} alt="icono único" className="button-icon" />
-                                    CREAR ENTREGAS
-                                </button>
-                            </Col>
-                        </Row>
+                            <>
+                                {(userAuth.rol_id === RolesEnum.ADMIN) && (
+                                    <Col xs={12} md={6} className="d-flex align-items-center">
+                                        <Select
+                                            value={selectedSupplier}
+                                            onChange={(selectedOption) => setSelectedSupplier(selectedOption)}
+                                            options={suppliers?.map((opt) => ({ value: opt.id, label: opt.nombre }))}
+                                            placeholder="Selecciona una compañía"
+                                            classNamePrefix="custom-select"
+                                            className="custom-select w-100"
+                                        />
+                                    </Col>
+                                )}
 
+                                {(userAuth.rol_id === RolesEnum.ADMIN || userAuth.rol_id === RolesEnum.SUPPLIER) && (
+                                    <Col
+                                        xs={12}
+                                        md={6}
+                                        className="d-flex align-items-center justify-content-md-start justify-content-center"
+                                    >
+                                        <button
+                                            onClick={handleCreateDeliveries}
+                                            className="deliveries-button deliveries d-flex align-items-center"
+                                        >
+                                            <img src={imgFrame2} alt="icono único" className="button-icon" />
+                                            CREAR ENTREGAS
+                                        </button>
+                                    </Col>
+                                )}
+                            </>
+                        </Row>
                     </Container>
                 </div>
 
@@ -730,6 +775,7 @@ export const Deliveries = () => {
 
                 <div className="">
                     <Container>
+                        <hr/>
                         {listDeliveriesToUser.length > 0 && !showDeliveryForm ? (
                             <>
                                 <DataGrid
