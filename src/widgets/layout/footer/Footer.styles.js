c49ebc54122645c25a7a7337341e4e26da4980f0
Edward Camilo Oviedo Lozano <edwardo@csfin.com>
2026-03-05 19:24:30 -0500

    chore(layout): include footer styles update

--- src/widgets/layout/footer/Footer.styles.js
+++ src/widgets/layout/footer/Footer.styles.js
@@ -1,18 +1,17 @@
 import styled from "@emotion/styled";
 
 export const FooterShell = styled.footer({
-  padding: "var(--space-5) var(--space-6) calc(var(--space-7) + var(--space-5))",
-  textAlign: "center",
+    padding: "var(--space-5) var(--space-6) calc(var(--space-7) + var(--space-5))",
+    textAlign: "center",
 });
 
-export const FooterImage = styled.img(({ theme }) => ({
-  display: "block",
-  width: "min(100%, 1100px)",
-  height: "auto",
-  marginInline: "auto",
-  borderRadius: "var(--radius-md)",
-  boxShadow: theme.colors.shadowSm,
-  marginBottom: "calc(var(--space-7) + var(--space-5))",
-  maxHeight: 260,
-  objectFit: "contain",
+export const FooterImage = styled.img(({theme}) => ({
+    display: "block",
+    width: "min(100%, 1100px)",
+    height: "auto",
+    marginInline: "auto",
+    borderRadius: "var(--radius-md)",
+    boxShadow: theme.colors.shadowSm,
+    maxHeight: 260,
+    objectFit: "contain",
 }));
