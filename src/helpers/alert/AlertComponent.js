754e1db38c316da288a961f44972b45c5201be79
Edward Camilo Oviedo Lozano <edwardo@csfin.com>
2026-05-06 16:34:30 -0500

    fix(login): streamline auth feedback on small screens

--- src/helpers/alert/AlertComponent.js
+++ src/helpers/alert/AlertComponent.js
@@ -1,53 +1,58 @@
 import Swal from "sweetalert2";
 
 class AlertComponent {
-    static success(title, message) {
+    static success(title, message, options = {}) {
         return Swal.fire({
             title: title,
             text: message,
             icon: "success",
             width: 300,
             heightAuto: true,
+            ...options,
         });
     }
 
-    static error(title, message) {
+    static error(title, message, options = {}) {
         return Swal.fire({
             title: title,
             text: message,
             icon: "error",
             width: 300,
             heightAuto: true,
+            ...options,
         });
     }
 
-    static Error(title, html) {
+    static Error(title, html, options = {}) {
         return Swal.fire({
             title: title,
             html: html,
             icon: "error",
             width: 360,
             heightAuto: true,
+            ...options,
         });
     }
 
-    static info(title, message) {
+    static info(title, message, options = {}) {
         return Swal.fire({
             title: title,
             text: message,
             icon: "info",
             width: 300,
             heightAuto: true,
+            ...options,
         });
     }
 
-    static warning(title, message) {
+    static warning(title, message, options = {}) {
         return Swal.fire({
             title: title,
             text: message,
             icon: "warning",
             width: 300,
             heightAuto: true,
+            ...options,
         });
     }
 }
